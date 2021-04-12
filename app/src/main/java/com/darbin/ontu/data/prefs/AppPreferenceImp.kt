af34a767098fe7c84c77b201ae895c8f1dace54f
akib khan <akib.khan@askiitians.com>
2021-04-12 19:11:05 +0530

    Normal Commit

--- app/src/main/java/com/darbin/ontu/data/prefs/AppPreferenceImp.kt
+++ app/src/main/java/com/darbin/ontu/data/prefs/AppPreferenceImp.kt
@@ -2,18 +2,18 @@ package com.darbin.ontu.data.prefs
 
 import android.content.SharedPreferences
 import com.darbin.ontu.data.models.UserModel
+import com.darbin.ontu.utils.PREF_KEY_USER
 import com.google.gson.Gson
 
 class AppPreferenceImp(private val mPrefs: SharedPreferences): AppPreferences {
-    private val PREF_KEY_USER = "PREF_KEY_USER"
 
     override fun getUser(): UserModel? {
         val userPref = mPrefs.getString(PREF_KEY_USER, null)
         return Gson().fromJson(userPref, UserModel::class.java)
     }
 
-    override fun setUser(userModel: UserModel?) {
-        val userPref = Gson().toJson(userModel)
+    override fun setUser(user: UserModel?) {
+        val userPref = Gson().toJson(user)
         mPrefs.edit().putString(PREF_KEY_USER, userPref).apply()
     }
 }
